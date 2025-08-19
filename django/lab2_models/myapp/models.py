4bf4e370184281a53aa3dc88b67fc8081b53b28e
DangThiMaiThuyen2003 <dangmaithuyen3002@gmail.com>
2025-08-20 02:03:36 +0700

    Querying Relationships and Complex Queries

--- django/lab2_models/myapp/models.py
+++ django/lab2_models/myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from datetime import date
+
+
 class PersonProfile(models.Model):   
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -157,6 +160,39 @@ class Membership(models.Model):
 
     def __str__(self):
         return f"{self.person} in {self.group}"
+    
+    
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField(default=date.today)
+    authors = models.ManyToManyField(Author)
+    number_of_comments = models.IntegerField(default=0)
+    number_of_pingbacks = models.IntegerField(default=0)
+    rating = models.IntegerField(default=5)
+
+    def __str__(self):
+        return self.headline
 
 
 
