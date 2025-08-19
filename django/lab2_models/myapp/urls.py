49f5bb6ba2ec889ae7a466a9adb9f2c752f88977
DangThiMaiThuyen2003 <dangmaithuyen3002@gmail.com>
2025-08-20 04:12:10 +0700

    create simple UI

--- django/lab2_models/myapp/urls.py
+++ django/lab2_models/myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from myapp.views import entry_list
+from myapp.views import entry_list, entry_delete, entry_detail, author_detail
 
 app_name = 'myapp'
 urlpatterns = [
     path('', entry_list, name='entry_list'),
+    path('delete/<int:pk>/', entry_delete, name='entry_delete'),
+    path('entry/<int:pk>/', entry_detail, name='entry_detail'),
+    path('author/<int:pk>/', author_detail, name='author_detail'),
 ]
--- django/lab2_models/myapp/views.py
+++ django/lab2_models/myapp/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from myapp.models import Entry
+from django.shortcuts import render, redirect, get_object_or_404
+from myapp.models import Entry, Author
+from django import forms
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['headline', 'body_text', 'pub_date', 'blog', 'authors']
 
 def entry_list(request):
     entries = Entry.objects.all()
-    return render(request, 'myapp/entry_list.html', {'entries': entries})
+    form = EntryForm()
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('myapp:entry_list')
+    return render(request, 'myapp/entry_list.html', {'entries': entries, 'form': form})
+
+
+def entry_delete(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    entry.delete()
+    return redirect('myapp:entry_list')
+
+def entry_detail(request, pk):
+    entry = get_object_or_404(Entry, pk=pk)
+    return render(request, 'myapp/entry_detail.html', {'entry': entry})
+
+def author_detail(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    entries = author.entry_set.all()
+    return render(request, 'myapp/author_detail.html', {'author': author, 'entries': entries})
